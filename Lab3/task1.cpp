e8a845f1c339a9e72d5d2450de5687c1f8fd1112
Anh Nguyen <44261379+BatmanofZuhandArrgh@users.noreply.github.com>
2019-09-18 17:08:42 -0400

    Add files via upload

---
+++ Lab3/task1.cpp
@@ -0,0 +1,105 @@
+//
+//  Task1.cpp
+//  Lab3
+//
+//  Created by Macbook on 9/18/19.
+//  Copyright © 2019 Anh Nguyen. All rights reserved.
+//
+
+# include "task1.h"
+# include<iostream>
+# include <math.h>
+#define PI 3.14159265
+
+using namespace std;
+//Complex::Complex()=default;
+
+Complex::Complex()
+{
+    real=0;
+    imaginary=0;
+}
+
+Complex::Complex(double x, double y)
+{
+    x=real;
+    y=imaginary;
+}
+
+Complex::Complex(double x)
+{
+    x=real;
+    imaginary=0;
+}
+
+void Complex::print()
+{
+    cout<<real<<"+"<<imaginary<<"i"<<endl;
+}
+
+double Complex::getReal()
+{
+    return real;
+}
+
+double Complex::getImaginary()
+{
+    return imaginary;
+}
+
+void Complex::setReal(double x)
+{
+    real=x;
+}
+
+void Complex::setImaginary(double y)
+{
+    imaginary=y;
+}
+
+double Complex::getlength()
+{
+    return sqrt(imaginary*imaginary+real*real);
+}
+
+double Complex::getPhi()
+{
+    return ((acos(imaginary/real))* 180.0 / 3.14159265);
+}
+
+Complex Complex::operator +(Complex &c)
+{
+    Complex add;
+    add.real=real+c.real;
+    add.imaginary=imaginary+c.imaginary;
+    return add;
+}
+
+Complex Complex::operator -(Complex &c)
+{
+    Complex sub;
+    sub.real=real-c.real;
+    sub.imaginary=imaginary-c.imaginary;
+    return sub;
+}
+
+Complex Complex::operator *(Complex &c)
+{
+    Complex mult;
+    mult.real=real*c.real-imaginary*c.imaginary;
+    mult.imaginary=real*c.imaginary-c.real*imaginary;
+    return mult;
+}
+
+Complex Complex::operator /(Complex &c)
+{
+    Complex div;
+    div.real=(real*c.real+imaginary*c.imaginary)/(c.real*c.real+c.imaginary*c.imaginary);
+    div.imaginary=(imaginary*c.real+real*c.imaginary)/(c.real*c.real+c.imaginary*c.imaginary);
+    return div;
+}
+
+bool Complex::operator ==(Complex &c)
+{
+    return ((imaginary==c.imaginary)&&(real==c.real));
+}
---
+++ Lab3/task1.h
@@ -0,0 +1,42 @@
+//
+//  Task1.hpp
+//  Lab3
+//
+//  Created by Macbook on 9/18/19.
+//  Copyright © 2019 Anh Nguyen. All rights reserved.
+//
+
+#include <stdio.h>
+#ifndef TASK1_H
+#define TASK1_H
+#include <iostream>
+#include <math.h>
+
+#define PI 3.14159265
+
+class Complex
+{
+public:
+    
+    Complex();
+    Complex(double x, double y);
+    Complex(double x);
+    
+    double getReal();
+    double getImaginary();
+    void setReal(double x);
+    void setImaginary(double y);
+    void print();
+    //friend ostream &operator<<(ostream &output, Complex &c);
+    Complex operator+(Complex &c);
+    Complex operator-(Complex &c);
+    Complex operator*(Complex &c);
+    Complex operator/(Complex &c);
+    bool operator==(Complex &c);
+    double getlength();
+    double getPhi();
+private:
+    double real,imaginary;
+};
+
+#endif /* Task1_hpp */
