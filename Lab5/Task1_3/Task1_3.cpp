bf9747e930700265f7ceabcf5dd9cd00ca2793d3
Anh Nguyen <44261379+BatmanofZuhandArrgh@users.noreply.github.com>
2019-10-08 19:14:53 -0400

    Create Task1_3.cpp

---
+++ Lab5/Task1_3/Task1_3.cpp
@@ -0,0 +1,72 @@
+// Data Structures Laboratory
+// Section 004
+// Lab 5
+// 10/3/2019
+// Group 6
+// Group Members: Anh Nguyen and Michael Stephens
+
+#include <iostream>
+#include "shelf.h"
+#include "BaseClassGame.h"
+#include "DerivedClassBoardGame.h"
+#include "DerivedClassVideoGame.h"
+#include "fullShelf.h"
+#include "emptyShelf.h"
+using namespace std;
+
+int main()
+{
+    shelf a;
+    while(true)
+    {
+        
+        int choice;
+        cout<<"\nPress 1 to add a board game to the shelf.\nPress 2 remove a board game from the shelf.\nPress 3 see how many board games are currently on the shelf.\nPress 4 to quit."<<endl;
+        cin>>choice;
+        
+        if(choice==1)
+        {
+            //int size;
+            int numPlayers;
+            int score;
+            game* b=new boardgame;
+            
+            cout<<"\nEnter number of players: ";
+            cin>>numPlayers;
+            cout<<"Enter score: ";
+            cin>>score;
+            b->setNumPlayers(numPlayers);
+            b->setScore(score);
+            
+            try
+            {
+                a.add(*b);
+            }
+            catch(const fullShelf& e)
+            {
+                cout<<e.what()<<'\n';
+            }
+            
+        }
+        else if(choice==2)
+        {
+            try
+            {
+                a.remove();
+            }
+            catch(const emptyShelf& e)
+            {
+                cout << e.what() << '\n';
+            }
+        }
+        else if(choice==3)
+        {
+            cout<<"\nThere are currently "<<a.getnumStored()<<" boardgames on the shelf."<<endl;
+        }
+        else if(choice==4)break;
+        
+        
+    }
+    
+    return 0;
+}
